f881431e9ccbb55e33cb2e14814d99f7eeb8e93b
jyotibhushan122 <39113232+jyotibhushan122@users.noreply.github.com>
2019-12-10 12:39:22 +0530

    Update MovieServiceImpl.java

--- book_my_show/src/main/java/org/movie/serviceImpl/MovieServiceImpl.java
+++ book_my_show/src/main/java/org/movie/serviceImpl/MovieServiceImpl.java
@@ -1,20 +1,18 @@
 package org.movie.serviceImpl;
 
+import java.math.BigInteger;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
-import org.movie.constent.SeatStatus;
 import org.movie.dao.MovieDAO;
 import org.movie.dao.SeatDao;
 import org.movie.dto.MovieDTO;
-import org.movie.dto.SeatDTO;
 import org.movie.mapper.MovieMapper;
 import org.movie.service.MovieService;
 import org.movie.vo.MovieVO;
 import org.movie.vo.ReportVo;
-import org.movie.vo.SeatVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,19 +21,18 @@ public class MovieServiceImpl implements MovieService {
 
 	@Autowired
 	private MovieMapper mapper;
-	
+
 	@Autowired
-	private MovieDAO movieDAO; 
-	
+	private MovieDAO movieDAO;
 	@Autowired
-	private SeatDao seatDAO;
-	
+	private SeatDao seatDao;
+
 	@Override
 	public void addMovie(MovieVO vo) {
 		MovieDTO dto = new MovieDTO();
 		dto = mapper.convertVO(vo);
 		movieDAO.save(dto);
-		
+
 	}
 
 	@Override
@@ -47,24 +44,25 @@ public class MovieServiceImpl implements MovieService {
 	}
 
 	@Override
-	public SeatVo updateStatus(MovieVO movieVo) {
-		Optional<SeatDTO> seatDTO = seatDAO.findBySeatId(movieVo.getSeatId());
-		if (seatDTO.get().isBookedStatus()) 
-			throw new Exception("seat already booked");
-		seatDTO.get().setTxId(UUID.randomUUID().toString());
-		seatDTO.get().setBookedStatus(Boolean.TRUE);
-		seatDTO.get().setSeatStatus(SeatStatus.SEAT_BOOKED);
-		seatDTO.get().setBookedDate(LocalDate.now());
-		seatDAO.save(seatDTO.get());
-		return mapper.getMapSeatDtoToVo(seatDTO.get());
-	}
-
-	@Override
 	public List<ReportVo> genReport(LocalDate fromDate, LocalDate toDate) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+		List<Object[]> listDate = seatDao.genReport(fromDate, toDate);
+		List<ReportVo> reportList = new ArrayList<>();
+		listDate.forEach(r -> {
+			ReportVo vo = new ReportVo();
+			if (r[0] != null)
+				vo.setSeat_booked(new BigInteger(r[0].toString()).longValue());
+			if (r[1] != null)
+				vo.setSeat_not_booked(new BigInteger(r[1].toString()).longValue());
+			if (r[2] != null)
+				vo.setTotal_revenue(new BigInteger(r[2].toString()).longValue());
+			if (r[3] != null)
+				vo.setTotal_loss(new BigInteger(r[3].toString()).longValue());
+			if (r[4] != null)
+				vo.setBookingDate(LocalDate.parse(r[4].toString()));
+			reportList.add(vo);
+		});
+		return reportList;
 
+	}
 
 }
